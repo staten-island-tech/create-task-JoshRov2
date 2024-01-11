e561cf4222b2fc8b7274ccb61e7848f91a7fa624
JoshRov2 <joshr16@nycstudents.net>
2024-01-11 09:22:39 -0500

    update

---
+++ main.js
@@ -0,0 +1,26 @@
+const appEl = document.getElementById('app');
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    return data.Results;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const quoteURL =`https://favqs.com/api/qotd?format=json`;
+
+function quoteGen(quote){
+    appEl.textContent = " ";
+    quote.forEach((result) => {
+        const HTML = `
+        <h2>${result.body}</h2>
+        <h3>Author: ${result.author}</h3>
+        `   
+    appEl.insertAdjacentHTML("beforeend", HTML)
+    });
+}
+quoteGen(quoteURL);
