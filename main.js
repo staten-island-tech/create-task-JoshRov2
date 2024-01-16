5f1b26c0b8dbcf2f6e403ccba4463b4f8886e070
JoshRov2 <joshr16@nycstudents.net>
2024-01-16 09:21:25 -0500

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const appEl = document.getElementById('app');
-
+const quoteBtn = document.getElementById('startup')
 async function getData(URL) {
   try {
     const response = await fetch(URL);
@@ -19,9 +19,18 @@ function quoteGen(quote){
         const HTML = `
         <h2>${result.body}</h2>
         <h3>Author: ${result.author}</h3>
+        <button id="next">Next Quote</button>
+        <button id="fave">Add to Favorites</button>
+        <button id="dis">Dislike</button>
         `   
     appEl.insertAdjacentHTML("beforeend", HTML)
     });
 }
 const quotes = await getData(quoteURL)
-quoteGen(quotes);
+quoteBtn.addEventListener("click",() =>{
+  const display = quotes.Results;
+  if(!display){
+    return false;
+  }
+  quoteGen(display);
+});
