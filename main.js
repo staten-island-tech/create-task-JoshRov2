07bcafa520388670ffa409ed860187d9975811ac
JoshRov2 <joshr16@nycstudents.net>
2024-01-25 15:09:11 -0500

    update

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const appEl = document.getElementById('app');
+const faveEl = document.getElementById('favorites')
+const disEl = document.getElementById('disliked')
 const favorites = [];
+const dislikedAuthors = [];
 const quoteBtn = document.getElementById('startup')
 
 async function getData(URL) {
@@ -7,12 +10,7 @@ async function getData(URL) {
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data);
-
-    if (Array.isArray(data) && data.length > 0) {
-      return data[0];
-    } else {
-      throw new Error("Invalid API Response")
-    }
+    return data[0];
   } catch (error) {
     console.error(error);
   }
@@ -32,7 +30,6 @@ function quoteGen(data) {
   const HTML = `
   <h2>${data.content}</h2>
   <h3>Author: ${data.author}</h3>
-  <button class="next">Next Quote</>
   <button class="fave">Add to Favorites</>
   <button class="dis">Dislike</>
   `;
@@ -40,16 +37,8 @@ function quoteGen(data) {
 }
 
 appEl.addEventListener("click", async(event) => {
-    if(event.target.matches(".next")){
-      console.log(quotes);
-  const nextQuote = quotes.Results;
-  if(!display){
-    return false;
-  }
-  quoteGen(nextQuote);
-    }
     if(event.target.matches(".fave")){
-    const faveQuote = await fetchData(quotes.Results, favorites)
+    const faveQuote = await getData(quotes.Results, favorites)
     favorites.push(faveQuote);
     }
     if(event.target.matches(".dis")){
