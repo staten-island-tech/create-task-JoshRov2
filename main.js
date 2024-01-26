5e8146a70eed28791234581dc9b52bda4030303c
JoshRov2 <joshr16@nycstudents.net>
2024-01-25 20:33:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,21 @@ async function getData(URL) {
     console.error(error);
   }
 };
-
-quoteBtn.addEventListener("click", async () => {
-  const quoteURL = `https://api.quotable.io/quotes/random?format=json`;
+const quoteURL = `https://api.quotable.io/quotes/random?format=json`;
+quoteBtn.addEventListener("click", getQuote);
+async function getQuote() {
   const quotes = await getData(quoteURL);
   if (!quotes) {
     return false;
   }
-  quoteGen(quotes);
-});
+  console.log(quotes)
+  if (dislikedAuthors.includes(quotes.author)){
+    console.log('if');
+    getQuote();
+  } else {
+    quoteGen(quotes);
+  }
+}
 
 function quoteGen(data) {
   appEl.innerHTML = "";
@@ -37,11 +43,28 @@ function quoteGen(data) {
 }
 
 appEl.addEventListener("click", async(event) => {
+  const el = event.target
     if(event.target.matches(".fave")){
-    const faveQuote = await getData(quotes.Results, favorites)
-    favorites.push(faveQuote);
+    const quoteContent = {text: el.parentElement.children[0].textContent, author: el.parentElement.children[1].textContent};    
+    console.log(quoteContent);
+    favorites.push(quoteContent);
+    console.log(favorites);
+    faveQuotes();
     }
     if(event.target.matches(".dis")){
-      
+    const quoteAuthor = el.parentElement.children[1].textContent;
+    const name = quoteAuthor.substring(8);
+    console.log(name);
+    dislikedAuthors.push(name);
     }
   });
+
+  function faveQuotes(){    
+  faveEl.innerHTML = " ";
+  favorites.forEach((quote) => {
+    const HTML = `
+    <h2>${quote.text}</h2>
+    <h3>${quote.author}</h3>
+    `
+    faveEl.insertAdjacentHTML("beforeend", HTML)
+  })}
